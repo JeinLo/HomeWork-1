8d4fa92e73fc689c0bf2469f88099b19c7e301af
Milana <milana.milana@gmail.com>
2024-12-21 19:21:40 +0300

    корректировка изображений

--- games/gamesSix.js
+++ games/gamesSix.js
@@ -1,16 +1,24 @@
-
+// Получаем кнопки по их ID
 const colorButton = document.getElementById('colorButton');
+const resetButton = document.getElementById('resetButton');
 
+// Функция для генерации случайного цвета в формате RGB
 function getRandomColor() {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
-    
     return `rgb(${red}, ${green}, ${blue})`;
 }
 
-function gameSix() {
+// Функция, которая будет вызываться при нажатии на кнопку смены цвета
+function changeBackgroundColor() {
     const randomColor = getRandomColor();
-    document.body.style.backgroundColor = randomColor;
+    document.body.style.backgroundColor = randomColor; // Изменяем цвет фона страницы
 }
-colorButton.addEventListener('click', gameSix);
+
+function resetBackgroundColor() {
+    document.body.style.backgroundColor = 'white'; 
+}
+
+colorButton.addEventListener('click', changeBackgroundColor);
+resetButton.addEventListener('click', resetBackgroundColor); 
